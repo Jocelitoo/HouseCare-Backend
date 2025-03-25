7b9dca95a64e5d22e41ae118894f93ae8a8592da
Jocelito Climeres <juninhoclimeres@outlook.com>
2025-03-25 13:14:07 -0300

    build port

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { routes } from "./routes/indexRoutes";
 const app = express();
 app.use(express.json());
 app.use(cors());
+const port = process.env.PORT || 4000;
 
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello world!" });
@@ -12,6 +13,6 @@ app.get("/test", async (req: Request, res: Response) => {
 
 app.use(routes);
 
-app.listen(7000, () => {
-  console.log("Server started on localhost:7000");
+app.listen(port, () => {
+  console.log(`Server started on localhost:${port}`);
 });
